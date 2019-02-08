21c3b39991ad04df9a870f3592be5ad20b76fa01
doter <doter@192.168.4.18>
2019-02-08 17:01:34 +0100

    Afegit metode per crear form.

--- public_html/creacioFormulari.js
+++ public_html/creacioFormulari.js
@@ -1,7 +1,30 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 
+var botoGenerarFormulari = document.getElementById("generarFormulari");
+var titolFormulari = document.getElementById("nomFomrulari");
+
+var countForm = 0;
+
+botoGenerarFormulari.addEventListener("click", esPotCrear);
+
+//Comproba que existeixi un titol per el formulari, sino no el crea.
+function esPotCrear() {
+    if (titolFormulari.value != null || titolFormulari.value != "") {
+        generarFormulari(titolFormulari.value);
+        titolFormulari.value = "";
+    } else {
+        alert("No has possat titol.");
+    }
+}
+
+function generarFormulari(titol) {
+    var body = document.getElementsByTagName("body")[0];
+
+    var form = document.createElement("form");
+
+    var contingut = document.createTextNode(titol);
+    
+    form.appendChild(contingut);
+
+    body.appendChild(form);
+}
