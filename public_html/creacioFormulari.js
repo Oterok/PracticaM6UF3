784b161d398600e097ce40ca2bd7ce15a1a44c9f
doter <doter@192.168.4.18>
2019-02-18 21:52:49 +0100

    Elimant el boto de generar i generat elements en funcio del fomr selecionat

--- public_html/creacioFormulari.js
+++ public_html/creacioFormulari.js
@@ -5,13 +5,22 @@ var contenidorforms = document.getElementById("contenidorforms");
 var body = document.getElementsByTagName("body")[0];
 
 var countForm = 0;
-var llistatIdForm = [];
-var botoOcult = false;
+var llistatForm = [];
 var arrayInputs = [[]];
+var idFormACtual;
 
 botoGenerarFormulari.addEventListener("click", esPotCrear);
-botoelementSeleccionat.addEventListener("click", elementSeleccionat);
-body.addEventListener("click", mostrarBotoFormulari);
+//botoelementSeleccionat.addEventListener("click", elementSeleccionat);
+document.getElementById("Input").addEventListener("click", agregarInput);
+document.getElementById("Text").addEventListener("click", agregarText);
+document.getElementById("Data").addEventListener("click", agregarData);
+document.getElementById("Fitxer").addEventListener("click", agregarFitxer);
+document.getElementById("Rang").addEventListener("click", agregarRang);
+document.getElementById("Email").addEventListener("click", agregarEmail);
+document.getElementById("Radio").addEventListener("click", agregarRadio);
+document.getElementById("Check").addEventListener("click", agregarCheckbox);
+
+body.addEventListener("click", seleccionarFormulari);
 
 //Comproba que existeixi un titol per el formulari, sino no el crea.
 function esPotCrear() {
@@ -40,7 +49,7 @@ function generarFormulari(titol) {
     form.appendChild(ultimaLinea);
     contenidorforms.appendChild(form);
 
-    llistatIdForm[countForm] = form;
+    llistatForm[countForm] = form;
 
     countForm++;
     //Genera el esdeveniment per el form que es genera.
@@ -51,139 +60,132 @@ function generarFormulari(titol) {
 
 }
 
-function mostrarBotoFormulari(e) {
-    if (!botoOcult) {
-        if (e.srcElement.id.substring(0, 4) == "form") {
-            generaLlista(document.getElementById(e.srcElement.id));
-            botoOcult = true;
+function seleccionarFormulari(e) {
+    if (e.srcElement.id.substring(0, 4) == "form") {
+        if(idFormACtual != null){
+        document.getElementById(idFormACtual).style.border = "thick solid #FFFFFF";
         }
-    } else {
-        ocultarBotoFormulari(document.getElementById(e.target.id));
+        //document.getElementById(e.srcElement.id.substring).style='border:10p x solid #CCCCCC;';
+        document.getElementById(e.srcElement.id).style.border = "thick solid #CCCCCC";
+        idFormACtual = e.srcElement.id;
+        botoOcult = true;
     }
 }
 
-function ocultarBotoFormulari(form) {
-    if (botoOcult) {
-        form.lastChild.remove();
-        botoOcult = false;
-    }
-}
 
-function generaLlista(form) {
-    var opcions = ["Input", "Text", "Data", "Fitxer", "Rang", "Email", "Radios", "Checkbox"]; //Valors que l'usuari pot seleccionar per omplir el seu formulari
-    var seleccio = document.createElement("select"); //Creem el element select
 
-    seleccio.id = "seleccio" + (countForm - 1); //Posem el ID a la llista. Potser cal que es canvii el "ID" de cada select per cada formulari?
-    form.appendChild(seleccio); //Afegim el select al formulari
 
-    for (var i = 0; i < opcions.length; i++) {
-        var option = document.createElement("option");
-        option.value = opcions[i];
-        option.text = opcions[i];
-        seleccio.appendChild(option);
-    }
+
+function agregarInput(e) {
+    
+    var f = idFormACtual.substring(4,5);
+    var generaInput = document.createElement("input");
+    generaInput.setAttribute("type", "text");
+    generaInput.setAttribute("id", "form-" + f + "-input-" + arrayInputs[f][0]);
+    llistatForm[f].appendChild(generaInput);
+    arrayInputs[f][0]++;
 
 }
 
+function agregarText(e) {
+    var f = idFormACtual.substring(4,5);
+    var generaText = document.createElement("input");
+    generaText.setAttribute("type", "text");
+    generaText.setAttribute("id", "form-" + f + "-text-" + arrayInputs[f][1]);
+    llistatForm[f].appendChild(generaText);
+    arrayInputs[f][1]++;
 
-function elementSeleccionat() {
+}
 
-    var e = document.getElementById("seleccio" + (countForm - 1)).value;
-    var f = countForm - 1;
+function agregarData(e) {
+    var f = idFormACtual.substring(4,5);
+    var generaData = document.createElement("input");
+    generaData.setAttribute("type", "date");
+    generaData.setAttribute("id", "form-" + f + "-date-" + arrayInputs[f][2]);
+    llistatForm[f].appendChild(generaData);
+    arrayInputs[f][2]++;
 
-    if (e === "Input") {
-        var generaInput = document.createElement("input");
-        generaInput.setAttribute("type", "text");
-        generaInput.setAttribute("id", "form-" + f + "-input-" + arrayInputs[f][0]);
-        form.appendChild(generaInput);
-        arrayInputs[f][0]++;
+}
 
-    } else if (e === "Text") {
-        var generaText = document.createElement("input");
-        generaText.setAttribute("type", "text");
-        generaText.setAttribute("id", "form-" + f + "-text-" + arrayInputs[f][1]);
-        form.appendChild(generaText);
-        arrayInputs[f][1]++;
+function agregarFitxer(e) {
+    var f = idFormACtual.substring(4,5);
+    var generaFitxer = document.createElement("input");
+    generaFitxer.setAttribute("type", "file");
+    generaFitxer.setAttribute("id", "form-" + f + "-file-" + arrayInputs[f][3]);
+    llistatForm[f].appendChild(generaFitxer);
+    arrayInputs[f][3]++;
 
-    } else if (e === "Data") {
-        var generaData = document.createElement("input");
-        generaData.setAttribute("type", "date");
-        generaData.setAttribute("id", "form-" + f + "-date-" + arrayInputs[f][2]);
-        form.appendChild(generaData);
-        arrayInputs[f][2]++;
+}
 
-    } else if (e === "Fitxer") {
+function agregarRang(e) {
+    var f = idFormACtual.substring(4,5);
+    var minim = prompt("Introdueix el valor minim", "1");
+    var maxim = prompt("Introdueix el valor maxim", "10");
+    var generaRang = document.createElement("input");
 
-        var generaFitxer = document.createElement("input");
-        generaFitxer.setAttribute("type", "file");
-        generaFitxer.setAttribute("id", "form-" + f + "-file-" + arrayInputs[f][3]);
-        form.appendChild(generaFitxer);
-        arrayInputs[f][3]++;
+    generaRang.setAttribute("type", "number");
+    generaRang.setAttribute("id", "form-" + f + "-rang-" + arrayInputs[f][4]);
+    generaRang.setAttribute("min", minim);
+    generaRang.setAttribute("max", maxim);
+    llistatForm[f].appendChild(generaRang);
+    arrayInputs[f][4]++;
 
-    } else if (e === "Rang") {
+}
 
-        var minim = prompt("Introdueix el valor minim", "1");
-        var maxim = prompt("Introdueix el valor maxim", "10");
-        var generaRang = document.createElement("input");
+function agregarEmail(e) {
+    var f = idFormACtual.substring(4,5);
+    var generaEmail = document.createElement("input");
+    generaEmail.setAttribute("type", "email");
+    generaEmail.setAttribute("id", "form-" + f + "-emails-" + arrayInputs[f][5]);
+    llistatForm[f].appendChild(generaEmail);
+    arrayInputs[f][5]++;
 
-        generaRang.setAttribute("type", "number");
-        generaRang.setAttribute("id", "form-" + f + "-rang-" + arrayInputs[f][4]);
-        generaRang.setAttribute("min", minim);
-        generaRang.setAttribute("max", maxim);
-        form.appendChild(generaRang);
-        arrayInputs[f][4]++;
+}
 
-    } else if (e === "Email") {
+function agregarRadio(e) {
+    var f = idFormACtual.substring(4,5);
+    var quantitatRadios = prompt("Introdueix quants radios vols generar", "3");
+    var nomRadios = prompt("Introdueix el nom d'aquesta grup d'opcions (p.ex: cotxes).", "cotxes");
 
-        var generaEmail = document.createElement("input");
-        generaEmail.setAttribute("type", "email");
-        generaEmail.setAttribute("id", "form-" + f + "-emails-" + arrayInputs[f][5]);
-        form.appendChild(generaEmail);
-        arrayInputs[f][5]++;
+    for (var i = 0; i < quantitatRadios; i++) {
 
-    } else if (e === "Radios") {
+        var valorRadio = prompt("Introdueix el text per el radio numero" + i + ": ", "Opció " + i);
 
-        var quantitatRadios = prompt("Introdueix quants radios vols generar", "3");
-        var nomRadios = prompt("Introdueix el nom d'aquesta grup d'opcions (p.ex: cotxes).", "cotxes");
+        var generaRadio = document.createElement("input");
 
-        for (var i = 0; i < quantitatRadios; i++) {
-
-            var valorRadio = prompt("Introdueix el text per el radio numero" + i + ": ", "Opció " + i);
-
-            var generaRadio = document.createElement("input");
-
-            generaRadio.setAttribute("type", "radio");
-            generaRadio.setAttribute("id", "form-" + f + "-radio-" + arrayInputs[f][6]);
-            generaRadio.setAttribute("name", nomRadios);
-
-            var generaLabel = document.createElement("label");
-            form.appendChild(generaLabel);
-            generaLabel.innerHTML = valorRadio;
-
-            form.appendChild(generaRadio);
-            generaRadio.innerHTML = valorRadio;
-            arrayInputs[f][6]++;
-
-        }
-    } else if (e === "Checkbox") {
-
-        var nomCeckbox = prompt("Introdueix el nom d'aquesta grup d'opcions (p.ex: cotxes).", "cotxes");
-        var valorCheckbox = prompt("Introdueix el text per l'opció d'aquest checkbox", "text");
-
-        var generaCheckbox = document.createElement("input");
-        generaCheckbox.setAttribute("type", "checkbox");
-        generaCheckbox.setAttribute("id", "form-" + f + "-checkbox-" + arrayInputs[f][7]);
-        generaCheckbox.setAttribute("name", nomCeckbox);
-        generaCheckbox.setAttribute("value", nomCeckbox);
+        generaRadio.setAttribute("type", "radio");
+        generaRadio.setAttribute("id", "form-" + f + "-radio-" + arrayInputs[f][6]);
+        generaRadio.setAttribute("name", nomRadios);
 
         var generaLabel = document.createElement("label");
-        form.appendChild(generaLabel);
-        generaLabel.innerHTML = nomCeckbox;
+        llistatForm[f].appendChild(generaLabel);
+        generaLabel.innerHTML = valorRadio;
 
-        form.appendChild(generaCheckbox);
-        generaCheckbox.innerHTML = nomCeckbox;
-        arrayInputs[f][7]++;
+        llistatForm[f].appendChild(generaRadio);
+        generaRadio.innerHTML = valorRadio;
+        arrayInputs[f][6]++;
 
     }
-    console.log(arrayInputs);
 }
+
+function agregarCheckbox(e) {
+    var f = idFormACtual.substring(4,5);
+    var nomCeckbox = prompt("Introdueix el nom d'aquesta grup d'opcions (p.ex: cotxes).", "cotxes");
+    var valorCheckbox = prompt("Introdueix el text per l'opció d'aquest checkbox", "text");
+
+    var generaCheckbox = document.createElement("input");
+    generaCheckbox.setAttribute("type", "checkbox");
+    generaCheckbox.setAttribute("id", "form-" + f + "-checkbox-" + arrayInputs[f][7]);
+    generaCheckbox.setAttribute("name", nomCeckbox);
+    generaCheckbox.setAttribute("value", nomCeckbox);
+
+    var generaLabel = document.createElement("label");
+    llistatForm[f].appendChild(generaLabel);
+    generaLabel.innerHTML = nomCeckbox;
+
+    llistatForm[f].appendChild(generaCheckbox);
+    generaCheckbox.innerHTML = nomCeckbox;
+    arrayInputs[f][7]++;
+
+}
+
